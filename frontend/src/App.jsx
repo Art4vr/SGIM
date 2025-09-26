9f0ee767e0643b9882813f8c740fd1ed3a3bb090
CarlosM30 <mucarlosalberto@gmail.com>
2025-09-25 19:07:03 -0600

    Prueba commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Importa tus componentes de auth
+import Home from './screens/auth/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<h2 style={{ textAlign: 'center', marginTop: '50px' }}>Página no encontrada</h2>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/auth/Home.jsx
@@ -0,0 +1,34 @@
+import { useNavigate } from 'react-router-dom';
+import styles from '../../styles/auth/Home.module.css';
+
+/**
+ * Componente de pagina de inicio de la aplicación
+ * 
+ * Este componente da la bienvenida a los usuarios y le da las opciones de ir al
+ * apartado de Iniciar sesión o Registrar un nuevo usuario.
+ * 
+ * @component
+ * @returns {JSX.Element} Pagina de inicio de la aplicación
+ */
+const Home = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div
+            className={styles.bodyContainer}
+            style={{ backgroundImage: 'url(/imagenes/FondoMK.PNG)' }}
+        >
+            <div className={styles.homeContainer}>
+                <div className={styles.avatar}>
+                    <img src="/imagenes/MKSF.png" alt="Avatar" />
+                </div>
+                <h1 className={styles.title}>MAKIMANAGE</h1>
+                <h2 className={styles.subtitle}>BIENVENIDO</h2>
+                <button className={styles.loginBtn} onClick={() => navigate('/login')}>INICIAR</button>
+                <button className={styles.registerBtn} onClick={() => navigate('/register')}>REGISTRAR</button>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
