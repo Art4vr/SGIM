f2e885c927e20112a8589d3c29d00abf8d74b07a
Endira-Ledesma <endira.ledesma03@gmail.com>
2025-10-09 00:35:52 -0600

    Implementacion de accesos controlados y Registro de Imprevistos
    Ya está listo el back de generacion de imprevistos, ademas de que unicamente son realizados por el rol del chef
    Hay accesos controlados de acuerdo al rol y la url a la que pueda acceder

---
+++ backend/src/Routes/imprevistosRutas.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import { authMiddleware } from '../Middleware/authMiddleware.js';
+import { requirePermission, requireRole } from '../Middleware/roleMiddleware.js';
+
+import { nuevoImprevistoController, consultaImprevistoController } from '../Controllers/imprevistoControlador.js';
+
+const router = express.Router();
+
+//Registrar Imprevisto -> Chef - Rol 3
+router.post('/crear', authMiddleware, requireRole([3]), nuevoImprevistoController);
+
+//Listar Imprevistos -> Gerente | Chef - Rol 1|3
+router.get('/', authMiddleware, requireRole([1,3]), consultaImprevistoController);
+
+export default router;
