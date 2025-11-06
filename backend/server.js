3e22e3ddaa7a12e789f13a6a42e57be743c42cb6
Art4vr <madrid.martinez.arturo@gmail.com>
2025-11-05 19:33:10 -0600

    Corrección de un bug que no mostraba el menú

--- backend/server.js
+++ backend/server.js
@@ -35,8 +35,8 @@ app.use(globalLimiter);
 //const PORT = process.env.PORT || 5000;
 
 //aqui se define el prefijo para las rutas
-app.use('/api',router);
 app.use('/api/platillos',menuRutas);
+app.use('/api',router);
 
 //app.listen(PORT,()=>{
 //    console.log('<Servidor escuchando en el puerto', PORT + '>');
