578ae670d0adb83a3469ece943455c1dc4682fa4
CarlosM30 <mucarlosalberto@gmail.com>
2025-11-08 19:12:48 -0600

    Actualizacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import React, {useEffect, useState} from 'react'
+//import React, {useEffect, useState} from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
