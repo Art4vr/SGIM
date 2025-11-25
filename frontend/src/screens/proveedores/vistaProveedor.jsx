2ec514158e0b6a3f1826e784cdf5abfa065c7920
Art4vr <madrid.martinez.arturo@gmail.com>
2025-11-24 19:55:07 -0600

    Corrección del scrollbar y botón de volver

--- frontend/src/screens/proveedores/vistaProveedor.jsx
+++ frontend/src/screens/proveedores/vistaProveedor.jsx
@@ -111,7 +111,7 @@ const VistaProveedores = () => {
                         {cargando ? (
                             <p className={styles.loadingText}>🔄 Cargando proveedores...</p>
                         ) : (
-                            <div className={stylesCommon.tableWrapper}>
+                            <div className={stylesCommon.productTableWrapper}>
                                 <table className={styles.productTable}>
                                     <thead>
                                         <tr>
@@ -152,29 +152,26 @@ const VistaProveedores = () => {
                                         )}
                                     </tbody>
                                 </table>
-                                
-                                {user?.rol===1 &&(
-                                    <button
-                                        className={`${stylesCommon.registerBtn} ${stylesCommon.backBtn}`}
-                                        type="button"
-                                        onClick={() => navigate('/PanelGerente')}
-                                        >
-                                        VOLVER AL INICIO
-                                    </button>
-                                )}
-
-                                {user?.rol===2 &&(
-                                    <button
-                                        className={`${stylesCommon.registerBtn} ${stylesCommon.backBtn}`}
-                                        type="button"
-                                        onClick={() => navigate('/PanelEncargado')}
-                                        >
-                                        VOLVER AL INICIO
-                                    </button>
-                                )}
                             </div>
                         )}
-
+                        {user?.rol===1 &&(
+                        <button
+                            className={`${stylesCommon.registerBtn} ${stylesCommon.backBtn}`}
+                            type="button"
+                            onClick={() => navigate('/PanelGerente')}
+                        >
+                            VOLVER AL INICIO
+                        </button>
+                        )}
+                        {user?.rol===2 &&(
+                        <button
+                            className={`${stylesCommon.registerBtn} ${stylesCommon.backBtn}`}
+                            type="button"
+                            onClick={() => navigate('/PanelEncargado')}
+                        >
+                            VOLVER AL INICIO
+                        </button>
+                        )}
                         {modalVisible && (
                             <NuevoProveedor
                                 proveedor={proveedorEditando}
